04abdfe357e7181447fc3f637090f6599b672334
SongSang <941227_@naver.com>
2024-01-18 21:43:31 +0900

    refactor:styled component 위치수정, InputTextArea 함수 state 제거

--- src/components/atoms/InputTextArea/InputTextArea.jsx
+++ src/components/atoms/InputTextArea/InputTextArea.jsx
@@ -1,5 +1,20 @@
-import { useState } from 'react';
 import styled from 'styled-components';
+
+const InputTextArea = ({ width, height, placeholder, handler, value }) => {
+  const handleTextChange = (e) => {
+    handler(e.target.value);
+  };
+
+  return (
+    <Textarea
+      width={width}
+      height={height}
+      value={value}
+      onChange={handleTextChange}
+      placeholder={placeholder}
+    />
+  );
+};
 
 const Textarea = styled.textarea`
   display: flex;
@@ -16,23 +31,4 @@ const Textarea = styled.textarea`
     border: 1px solid #542f1a;
   }
 `;
-
-const InputTextArea = ({ width, height, placeholder }) => {
-  const [value, setValue] = useState('');
-
-  const handleTextChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  return (
-    <Textarea
-      width={width}
-      height={height}
-      value={value}
-      onChange={handleTextChange}
-      placeholder={placeholder}
-    />
-  );
-};
-
 export default InputTextArea;

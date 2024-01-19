ac4d9e0dc7c017ded6cd7a92a0bdb339892a1e47
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-19 18:06:49 +0900

    :wrench: Add 누락 이미지-로고추가

--- src/assets/imageData.js
+++ src/assets/imageData.js
@@ -2,12 +2,14 @@ import PersonIcon from './img/input-icon-person.svg';
 import defaultProfile from './img/profile-ellipse-1.svg';
 import messageIcon from './img/icon-messages.svg';
 import box from './img/box.svg';
+import logo from './img/logo.svg';
 
 const imageData = {
   PersonIcon,
   defaultProfile,
   messageIcon,
   box,
+  logo,
 };
 
 export default imageData;

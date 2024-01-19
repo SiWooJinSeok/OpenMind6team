0da58f18cad44bb6efcb7d2c126e001ef57c4530
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-19 17:31:31 +0900

    :wrench: Add svgFile
    이미지 파일들 추가

--- src/assets/imageData.js
+++ src/assets/imageData.js
@@ -1,11 +1,13 @@
-import PersonIcon from './images/input-icon-person.svg';
+import PersonIcon from './img/input-icon-person.svg';
 import defaultProfile from './img/profile-ellipse-1.svg';
 import messageIcon from './img/icon-messages.svg';
+import box from './img/box.svg';
 
 const imageData = {
   PersonIcon,
   defaultProfile,
   messageIcon,
+  box,
 };
 
 export default imageData;

5792a5ca73b62d62f6fb776491f244f501c0f7d0
giho <giho7427@daum.net>
2024-01-19 18:12:50 +0900

    :poop: [fix] App.js 정화
    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import GlobalStyle from './GlobalStyle';
-import ThumbsDown from './components/atoms/Reaction/ThumbsDown';
-import ThumbsUp from './components/atoms/Reaction/ThumbsUp';
 
 function App() {
   return (
@@ -8,15 +6,6 @@ function App() {
       <GlobalStyle />
       <div>
         <h2>Hello World</h2>
-        <ThumbsUp isLiked count={12} />
-        <ThumbsUp isLiked={false} count={12} />
-        <ThumbsUp isLiked count={0} />
-        <ThumbsUp isLiked={false} count={0} />
-
-        <ThumbsDown isDisliked count={12} />
-        <ThumbsDown isDisliked={false} count={12} />
-        <ThumbsDown isDisliked count={0} />
-        <ThumbsDown isDisliked={false} count={0} />
       </div>
     </>
   );

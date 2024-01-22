fca6e744da99fef32fce058277a0f95b651fe539
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-22 14:59:13 +0900

    :lipstick: Feat: AnswerPageHeader UI완성

--- src/assets/imageData.js
+++ src/assets/imageData.js
@@ -3,6 +3,7 @@ import defaultProfile from './img/profile-ellipse-1.svg';
 import messageIcon from './img/icon-messages.svg';
 import box from './img/box.svg';
 import logo from './img/logo.svg';
+import bgImg from './img/v-872-batch-5-nunny-041.png';
 
 const imageData = {
   PersonIcon,
@@ -10,6 +11,7 @@ const imageData = {
   messageIcon,
   box,
   logo,
+  bgImg,
 };
 
 export default imageData;

4fcf73a833819db3107d388231072fbf9a716ef1
siyeol97 <leesi2830@naver.com>
2024-01-22 16:03:07 +0900

    🚚 Feat: 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
-import MainPage from './components/pages/MainPage';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <MainPage />
+      <Outlet />
     </>
   );
 }

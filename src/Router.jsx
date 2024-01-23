8ce807c5e03b4775acb82bfa47f93870105dc980
siyeol97 <leesi2830@naver.com>
2024-01-23 20:29:42 +0900

    🚚 Fix: route children 경로에서  '/' 제거

--- src/Router.jsx
+++ src/Router.jsx
@@ -15,15 +15,15 @@ const router = createBrowserRouter([
         element: <MainPage />,
       },
       {
-        path: '/list',
+        path: 'list',
         element: <QuestionListPage />,
       },
       {
-        path: '/post/:id/answer',
+        path: 'post/:id/answer',
         element: <AnswerPage />,
       },
       {
-        path: '/post/:id',
+        path: 'post/:id',
         element: <QuestionPage />,
       },
     ],

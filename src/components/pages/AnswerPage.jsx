4a00455382042b64a073b1e81f3183ad5e3c8b4b
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-25 16:25:39 +0900

    ✨  feat : 전체 삭제하기 버튼 기능 구현

--- src/components/pages/AnswerPage.jsx
+++ src/components/pages/AnswerPage.jsx
@@ -1,22 +1,35 @@
 import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import TopPanel from '../organisms/TopPanel/TopPanel';
 import DeleteButton from '../atoms/Button/DeleteButton';
 import FeedCardList from '../organisms/FeedCardList/FeedCardList';
 import useSubjectData from '../../hooks/useSubjectData';
+import requestApi from '../../utils/requestApi';
 
 // TODO(노진석) : 나중에 로직 만들 때 수정
 export default function AnswerPage() {
   const subjectId = useParams();
-  const { data } = useSubjectData(`subjects/${subjectId.id}/`);
+  const currentSubjectId = localStorage.getItem('subjectId');
+  const navigate = useNavigate();
+  const SUBJECT_URL = `subjects/${subjectId.id}/`;
+  if (subjectId.id !== currentSubjectId) {
+    navigate('/list');
+  }
+  const deleteSubject = () => {
+    requestApi(SUBJECT_URL, 'delete');
+    localStorage.removeItem('subjectId');
+    navigate('/');
+  };
+  const { data } = useSubjectData(SUBJECT_URL);
   const owner = { name: data?.name, imageSource: data?.imageSource };
+
   return (
     <>
       <TopPanel owner={owner} />
       <Wrapper>
         <Container>
           <DeleteButtonBox>
-            <DeleteButton />
+            <DeleteButton onClick={deleteSubject} />
           </DeleteButtonBox>
           <FeedCardList type="answer" questionCount={data?.questionCount} />
         </Container>

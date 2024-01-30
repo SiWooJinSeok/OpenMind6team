4c173512829535620304f25d2e66efe137ea0289
giho <giho7427@daum.net>
2024-01-30 16:42:14 +0900

    :arrow_down: [ Docs ] EditableDropdown 스토리북 완성
    docs

--- src/components/atoms/Dropdown/EditableDropdown.stories.jsx
+++ src/components/atoms/Dropdown/EditableDropdown.stories.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import EditableDropdown from './EditableDropdown';
+import imageData from '../../../assets/imageData';
 
 export default {
   title: 'Atoms/Dropdown/EditableDropdown',
   component: EditableDropdown,
 };
 
-export function name() {
-  return <EditableDropdown />;
+export function Default() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const firstChildren = {
+    onClick: () => {},
+    imageSource: imageData.editIcon,
+    text: '수정하기',
+  };
+
+  const secondChildren = {
+    onClick: () => {},
+    imageSource: imageData.closeIcon,
+    text: '삭제하기',
+  };
+
+  return (
+    <EditableDropdown
+      onClick={handleDropdownToggle}
+      isOpen={isDropdownOpen}
+      firstChildren={firstChildren}
+      secondChildren={secondChildren}
+    />
+  );
 }
